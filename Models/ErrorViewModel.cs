52931c6273fb0b6141d7f52da2c211bc09b462ea
Stephen Harper <sjharper79@gmail.com>
2024-01-28 13:14:27 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace learn.microsoft.com.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
